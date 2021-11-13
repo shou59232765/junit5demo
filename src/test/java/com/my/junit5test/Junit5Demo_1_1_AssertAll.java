07e0d4697ac300bab672542d456f0ef3392b37ac
soso.he <soso.he@wolaidai.com>
2021-11-13 12:05:03 +0800

    添加成员优化1

--- src/test/java/com/my/junit5test/Junit5Demo_1_1_AssertAll.java
+++ src/test/java/com/my/junit5test/Junit5Demo_1_1_AssertAll.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.api.Test;
 public class Junit5Demo_1_1_AssertAll {
 
     @Test
-    void addTest(){
+    void addTest() {
         AddCode a1 = new AddCode();
-        Assertions.assertEquals(a1.add(5,6),11,"失败");
+        Assertions.assertEquals(a1.add(5, 6), 11, "失败");
         AddCode a2 = new AddCode();
-        Assertions.assertEquals(a2.add(5,6),12,"失败");
+        Assertions.assertEquals(a2.add(5, 6), 12, "失败");
 
     }
 
-
 }
+
